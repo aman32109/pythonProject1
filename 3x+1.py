b415f1c72d46a9a643ec5884d4942c933427eeb1
aman32109 <amanshooja@gmail.com>
2022-01-29 06:18:47 +0400

    commit

--- 3x+1.py
+++ 3x+1.py
@@ -1,17 +1,6 @@
-# 3x+1
-no= int(input('enter number'))
-if int(no%2==0):
-    var_even= no/2
-    print(var_even)
-else :
-    var_odd:int=(no*3)+1
-    print(var_odd)
-
-
-
-    
-    
-    
-
-
-
+#dumbass listen carefully
+num=int(input("enter number"))
+if int(num%2==0):
+    print("this number is even")
+else:
+    print("this number odd")
